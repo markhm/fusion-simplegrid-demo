fe8d2ca559c63ccd917f35930a356b075a86d8eb
Mark <github@hissinkmuller.nl>
2021-01-23 19:13:17 +0100

    Activated shared-styles.js

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,7 +1,7 @@
 import { Router } from '@vaadin/router';
 import { routes } from './routes';
 import './styles/simplegrid-styles';
-// import './styles/shared-styles';
+import './styles/shared-styles';
 
 export const router = new Router(document.querySelector('#outlet'));
 router.setRoutes(routes);
