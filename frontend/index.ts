d2751edc7029b38463cad83edc513f0cf1a68685
Mark <github@hissinkmuller.nl>
2021-01-23 16:57:04 +0100

    Deactivated unused shared-styles

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,7 +1,8 @@
 import { Router } from '@vaadin/router';
 import { routes } from './routes';
-import './styles/shared-styles';
 import './styles/simplegrid-styles';
+// import './styles/shared-styles';
+
 
 export const router = new Router(document.querySelector('#outlet'));
 router.setRoutes(routes);
