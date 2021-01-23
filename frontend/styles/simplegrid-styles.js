ea8b09b6044df3e8de4bcb3b092226f526953e82
Mark <github@hissinkmuller.nl>
2021-01-23 17:04:03 +0100

    Removed unused import

--- frontend/styles/simplegrid-styles.js
+++ frontend/styles/simplegrid-styles.js
@@ -1,4 +1,8 @@
-import '@polymer/polymer/lib/elements/custom-style.js';
+// import '@polymer/polymer/lib/elements/custom-style.js';
+
+// eagerly import theme styles so as we can override them
+import '@vaadin/vaadin-lumo-styles/all-imports';
+
 const $_documentContainer = document.createElement('template');
 
 $_documentContainer.innerHTML = `<custom-style>
